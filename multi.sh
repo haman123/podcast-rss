616e04f87921819066d51eb041ea81c9aef3ce8d
haman123 <wybj128@gmail.com>
2019-11-14 15:14:20 +0900

    Update multi.sh

--- multi.sh
+++ multi.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-ALBUMID=([0]='144892' [1]='94154' [2]='63048')
+ALBUMID=([0]='144892' [1]='94154' [2]='63048' )
 RSSFILE=([0]='yyjc.xml' [1]='ruixi.xml' [2]='daming.xml')
 GRABBER="qingting_linux_amd64"
 
